8cdd6a00267458a73913e37514565d9b67cecc15
admin <gautamjena383@gmail.com>
2024-04-15 23:49:03 +0530

    first commit

--- interview.java
+++ interview.java
@@ -4,7 +4,7 @@ public class interview {
         System.out.println(0.1 * 2 == 0.2);
         System.out.println(0.1 * 3  == 0.1);
 
-        //my code is doing great
+        //my code is doing great ----
 
     }
 }
